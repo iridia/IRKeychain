0655362a49d8a3b6f9a6bd7693b63431484c2955
Evadne Wu <ev@monoceroi.com>
2010-12-30 21:55:48 +0800

    reformats IRKeychainDefines; adds irNSStringFromOSStatus()

--- IRKeychainDefines.h
+++ IRKeychainDefines.h
@@ -47,31 +47,31 @@ typedef enum {
 
 static inline NSString* NSStringFromIRKeychainItemKind (IRKeychainItemKind kind) {
 
-    switch (kind) {
+	switch (kind) {
 
-        case IRKeychainItemKindAny:
-            return @"IRKeychainItemKindAny";
+		case IRKeychainItemKindAny:
+			return @"IRKeychainItemKindAny";
 
-        case IRKeychainItemKindPassword:
-            return @"IRKeychainItemKindPassword";
+		case IRKeychainItemKindPassword:
+			return @"IRKeychainItemKindPassword";
 
-        case IRKeychainItemKindInternetPassword: 
-            return @"IRKeychainItemKindInternetPassword";
+		case IRKeychainItemKindInternetPassword: 
+			return @"IRKeychainItemKindInternetPassword";
 
-        case IRKeychainItemKindCertificate: 
-            return @"IRKeychainItemKindCertificate";
+		case IRKeychainItemKindCertificate: 
+			return @"IRKeychainItemKindCertificate";
 
-        case IRKeychainItemKindKey: 
-            return @"IRKeychainItemKindKey";
+		case IRKeychainItemKindKey: 
+			return @"IRKeychainItemKindKey";
 
-        case IRKeychainItemKindIdentity: 
-            return @"IRKeychainItemKindIdentity";
+		case IRKeychainItemKindIdentity: 
+			return @"IRKeychainItemKindIdentity";
 
-        default:
-            assert(NO);
-            
-    }
-    
+		default:
+			assert(NO);
+			
+	}
+	
 }
 
 
@@ -80,31 +80,31 @@ static inline NSString* NSStringFromIRKeychainItemKind (IRKeychainItemKind kind)
 
 static inline CFTypeRef SecClassFromIRKeychainItemKind(IRKeychainItemKind kind) {
 
-    switch (kind) {
-            
-        case IRKeychainItemKindAny:
-            return NULL;
-            
-        case IRKeychainItemKindPassword:
-            return kSecClassGenericPassword;
-            
-        case IRKeychainItemKindInternetPassword: 
-            return kSecClassInternetPassword;
-            
-        case IRKeychainItemKindCertificate: 
-            return kSecClassCertificate;
-            
-        case IRKeychainItemKindKey: 
-            return kSecClassKey;
-            
-        case IRKeychainItemKindIdentity: 
-            return kSecClassIdentity;
-            
-        default:
-            assert(NO);
-            
-    }    
-    
+	switch (kind) {
+			
+		case IRKeychainItemKindAny:
+			return NULL;
+			
+		case IRKeychainItemKindPassword:
+			return kSecClassGenericPassword;
+			
+		case IRKeychainItemKindInternetPassword: 
+			return kSecClassInternetPassword;
+			
+		case IRKeychainItemKindCertificate: 
+			return kSecClassCertificate;
+			
+		case IRKeychainItemKindKey: 
+			return kSecClassKey;
+			
+		case IRKeychainItemKindIdentity: 
+			return kSecClassIdentity;
+			
+		default:
+			assert(NO);
+			
+	}	
+	
 }
 
 
@@ -113,8 +113,42 @@ static inline CFTypeRef SecClassFromIRKeychainItemKind(IRKeychainItemKind kind) 
 
 static inline NSString* irNSStringFromOSStatus(OSStatus status) {
 
-//	FIXME: Implement
-	return nil;
+	switch (status) {
+
+		case errSecSuccess:			
+			return @"Succes";
+		
+		case errSecUnimplemented:
+			return @"Unimplemented";
+			
+		case errSecParam:
+			return @"Keychain query parameters invalid";
+
+		case errSecAllocate:
+			return @"Can’t allocate memory";
+			
+		case errSecNotAvailable:
+			return @"No keychain available";
+			
+		case errSecDuplicateItem:
+			return @"duplicate item exists";
+
+		case errSecItemNotFound:
+			return @"Item not found";
+			
+		case errSecInteractionNotAllowed:
+			return @"User interaction is not allowed";
+			
+		case errSecDecode:
+			return @"Unable to decode the provided data";
+			
+		case errSecAuthFailed:
+			return @"The user name or passphrase you entered is not correct";
+			
+		default:
+			return @"Unknown status";
+
+	}
 
 }
 
