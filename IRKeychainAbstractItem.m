12dd83f049a3843a4e6b1f1c6694fa5c494dbefc
Evadne Wu <ev@monoceroi.com>
2010-12-30 22:09:19 +0800

    adds more work on IRKeychainAbstractItem

--- IRKeychainAbstractItem.m
+++ IRKeychainAbstractItem.m
@@ -9,6 +9,74 @@
 #import "IRKeychainAbstractItem.h"
 
 
+@interface IRKeychainAbstractItem ()
+
+@property (nonatomic, readwrite, retain) NSString *identifier;
+
+@end
+
+
+
+
+
 @implementation IRKeychainAbstractItem
+
+- (id) init {
+
+	return [self initWithIdentifier:nil];
+
+}
+
+- (id) initWithIdentifier:(NSString *)inIdentifier {
+
+	self = [super init]; if (!self) return nil;
+	
+	self.identifier = inIdentifier;
+	
+	return self;
+
+}
+
+- (id) initWithCoreFoundationReference:(CFTypeRef)reference {
+
+//	FIXME
+	
+	return [self initWithIdentifier:nil];
+
+}
+
+- (void) dealloc {
+
+	self.identifier = nil;
+	
+	[super dealloc];
+
+}
+
+
+
+
+
+- (NSString *) description {
+
+	return [[super description] stringByAppendingFormat:@"(identifier = %@)", self.identifier];
+
+}
+
+
+
+
+
+- (BOOL) synchronizeWithError:(NSError **)error overwrittingOriginal:(BOOL)shouldOverwrite {
+
+	NSLog(@"Synchronizing keychain item %@", self);
+	
+	return NO;
+
+}
+
+
+
+
 
 @end
