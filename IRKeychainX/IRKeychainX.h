b05973a49316a898b4a5d2383a1fc12bd414deec
Evadne Wu <ev@monoceroi.com>
2011-12-18 00:36:16 +0800

    IRKeychainX

---
+++ IRKeychainX/IRKeychainX.h
@@ -0,0 +1,9 @@
+//
+//  IRKeychainX.h
+//  IRKeychainX
+//
+//  Created by Evadne Wu on 12/18/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ IRKeychainX/IRKeychainX.m
@@ -0,0 +1,9 @@
+//
+//  IRKeychainX.m
+//  IRKeychainX
+//
+//  Created by Evadne Wu on 12/18/11.
+//  Copyright (c) 2011 Iridia Productions. All rights reserved.
+//
+
+#import "IRKeychainX.h"
